11b1b98cdcce0a8738bc67e31d52f3925aece47a
surajmeche <surajmechesuraj@gmail.com>
2026-04-02 14:42:41 +0545

    Update PWA configuration and service worker

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,41 @@
-const CACHE_NAME = "shantiram-pwa-v1";
+const CACHE_NAME = "shantiram-pwa-v2";
 
 const urlsToCache = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./me.jpeg"
+  "/Shantiram-Meche/",
+  "/Shantiram-Meche/index.html",
+  "/Shantiram-Meche/style.css",
+  "/Shantiram-Meche/me.jpeg",
+  "/Shantiram-Meche/icons/icon-192.png",
+  "/Shantiram-Meche/icons/icon-512.png"
 ];
 
 // Install
-self.addEventListener("install", event => {
-  self.skipWaiting();
+self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
+  self.skipWaiting();
 });
 
 // Activate
-self.addEventListener("activate", event => {
-  event.waitUntil(self.clients.claim());
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 // Fetch
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(
+      (cached) => cached || fetch(event.request)
+    )
   );
 });
