e243ba70294b541cdf4cbf3e0f00fe7038a6e76c
surajmeche <surajmechesuraj@gmail.com>
2026-04-02 13:49:23 +0545

    Update index.html, manifest.json, and service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,28 @@
 const CACHE_NAME = "shantiram-pwa-v1";
 
 const urlsToCache = [
-  "/shantiram_meche/",
-  "/shantiram_meche/index.html",
-  "/shantiram_meche/style.css",
-  "/shantiram_meche/me.jpeg"
+  "./",
+  "./index.html",
+  "./style.css",
+  "./me.jpeg"
 ];
 
 // Install
 self.addEventListener("install", event => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Fetch (cache first)
+// Activate
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+// Fetch
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
