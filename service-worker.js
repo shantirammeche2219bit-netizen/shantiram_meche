786e5b984f98f55bc929957dd9fe26d56b137acc
surajmeche <surajmechesuraj@gmail.com>
2026-04-02 14:24:33 +0545

    Revert PWA enhancements and remove PNG icons

--- service-worker.js
+++ service-worker.js
@@ -1,70 +1,28 @@
-const CACHE_NAME = "shantiram-meche-pwa-v3";
+const CACHE_NAME = "shantiram-pwa-v1";
 
 const urlsToCache = [
-  "/shantiram_meche/",
-  "/shantiram_meche/index.html",
-  "/shantiram_meche/style.css",
-  "/shantiram_meche/me.jpeg",
-  "/shantiram_meche/manifest.json",
-  "/shantiram_meche/icon-192.png",
-  "/shantiram_meche/icon-512.png",
-  "/shantiram_meche/icon-192-maskable.png",
-  "/shantiram_meche/icon-512-maskable.png"
+  "./",
+  "./index.html",
+  "./style.css",
+  "./me.jpeg"
 ];
 
 // Install
 self.addEventListener("install", event => {
-  console.log("Service Worker installing...");
   self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      console.log("Cache opened:", CACHE_NAME);
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
 // Activate
 self.addEventListener("activate", event => {
-  console.log("Service Worker activating...");
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            console.log("Deleting old cache:", cacheName);
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    }).then(() => self.clients.claim())
-  );
+  event.waitUntil(self.clients.claim());
 });
 
 // Fetch
 self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") {
-    return;
-  }
   event.respondWith(
-    caches.match(event.request).then(response => {
-      if (response) {
-        console.log("Cache hit for:", event.request.url);
-        return response;
-      }
-      return fetch(event.request).then(response => {
-        if (!response || response.status !== 200 || response.type !== "basic") {
-          return response;
-        }
-        const responseToCache = response.clone();
-        caches.open(CACHE_NAME).then(cache => {
-          cache.put(event.request, responseToCache);
-        });
-        return response;
-      }).catch(() => {
-        console.log("Fetch failed, returning cache:", event.request.url);
-        return caches.match("/shantiram_meche/index.html");
-      });
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
